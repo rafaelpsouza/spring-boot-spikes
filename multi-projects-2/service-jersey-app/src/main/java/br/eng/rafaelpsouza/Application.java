735bda1c19054b9fd104c50d3b4f7f82ca28ce68
Rafael Souza <rafael.bnc@gmail.com>
2016-04-16 22:01:00 -0700

    spring boot actuator

--- multi-projects-2/service-jersey-app/src/main/java/br/eng/rafaelpsouza/Application.java
+++ multi-projects-2/service-jersey-app/src/main/java/br/eng/rafaelpsouza/Application.java
@@ -1,9 +1,10 @@
 package br.eng.rafaelpsouza;
 
 import java.io.IOException;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 
 /**
  *
@@ -14,7 +15,11 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) throws IOException {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .properties("management.contextPath:/resources")
+                .properties("spring.application.name:example-service")
+                .properties("info.version:1.0")
+                .listeners(new ApplicationPidFileWriter()).run();
     }
 
 }
--- multi-projects-2/service-jersey-app/src/main/java/br/eng/rafaelpsouza/config/JerseyConfig.java
+++ multi-projects-2/service-jersey-app/src/main/java/br/eng/rafaelpsouza/config/JerseyConfig.java
@@ -1,10 +1,12 @@
 package br.eng.rafaelpsouza.config;
 
 import br.eng.rafaelpsouza.endpoint.RestEndpoint;
+import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
+@ApplicationPath("example")
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
